7b08a9f5cc70eb7b294a9fc0ec076dcb58195d9f
Kentaro Kuribayashi <kentarok@gmail.com>
2016-08-31 03:59:17 +0900

    cosmetic change

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
 	"github.com/kentaro/tororo/parser"
 	"github.com/kentaro/tororo/vm"
+	"os"
+	"strings"
 )
 
 func main() {
-    result := parser.Parse(strings.NewReader(os.Args[1]))
-    fmt.Println(vm.Run(result))
+	result := parser.Parse(strings.NewReader(os.Args[1]))
+	fmt.Println(vm.Run(result))
 }
