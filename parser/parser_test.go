e5fd727f6d49fe6fc5771105888782ea11bf64ea
Kentaro Kuribayashi <kentarok@gmail.com>
2016-09-05 22:51:52 +0900

    test using `reflect.DeepEqual`

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -12,69 +13,56 @@ var tests = []struct {
 	{
 		"9999",
 		[]Stmt{
-			NumberExpr{
+			[]Expr{NumberExpr{
 				Literal: "9999",
-			},
+			}},
 		},
 	},
 	{
 		"9.999",
 		[]Stmt{
-			NumberExpr{
+			[]Expr{NumberExpr{
 				Literal: "9.999",
-			},
+			}},
 		},
 	},
 	{
 		"1 + 1",
 		[]Stmt{
-			BinOpExpr{
+			[]Expr{BinOpExpr{
 				Left:     NumberExpr{Literal: "1"},
 				Operator: '+',
 				Right:    NumberExpr{Literal: "1"},
-			},
+			}},
 		},
 	},
 	{
 		`"foo \"bar\" baz"`,
 		[]Stmt{
-			StringExpr{
+			[]Expr{StringExpr{
 				Literal: `"foo \"bar\" baz"`,
+			}},
+		},
+	},
+	{
+		`
+		class Foo {
+			"got true"
+		}`,
+		[]Stmt{
+			ClassStmt{
+				Name: "Foo",
+				Stmts: []Stmt{[]Expr{StringExpr{Literal: "\"got true\""}}},
 			},
 		},
 	},
-	// TODO: test
-	// {
-	// 	`
-	// 	class Foo {
-	// 		"got true"
-	// 	}`,
-	// 	[]Stmt{
-	// 		ClassStmt{
-	// 			Name: "Foo",
-	// 			Stmts: []Stmt{[]Expr{StringExpr{Literal: "got true"}}},
-	// 		},
-	// 	},
-	// },
 }
 
 func TestParse(t *testing.T) {
 	for i, test := range tests {
 		stmts := Parse(strings.NewReader(test.src))
-
-		for j, stmt := range stmts {
-			switch stmt.(type) {
-			case []Expr:
-				for _, expr := range stmt.([]Expr) {
-					if expr != test.ast[j] {
-						t.Errorf("case %d:\n\n%s\n\nactual:\t\t%#v\nexpected:\t%#v", i, test.src, expr, test.ast[j])
-					}
-				}
-			default:
-				if stmt != test.ast[j] {
-					t.Errorf("case %d:\n\n%s\n\nactual:\t\t%#v\nexpected:\t%#v", i, test.src, stmt, test.ast[j])
-				}
-			}
+		if !reflect.DeepEqual(stmts, test.ast) {
+			t.Errorf("case %d:\n\n%s\n\nactual:\t\t%#v\nexpected:\t%#v", i, test.src, stmts, test.ast)
 		}
 	}
 }
